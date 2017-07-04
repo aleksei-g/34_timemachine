e3fbb4c9317b0bbb5020e3b75cd8a03314613a78
aleksei-g <aleksei_g@outlook.com>
2017-07-04 14:39:27 +0300

    add show message every 30 seconds after 5 minutes

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 var TIMEOUT_IN_SECS = 5 * 60
-var TEMPLATE = '<h1><span id="timer-minutes">00</span>:<span id="timer-seconds">00</span></h1>'
+var TEMPLATE = '<h1 style="font: 20px/20px Helvetica Neue,Arial,sans-serif;' +
+    'color: #6c797f; margin-top: 3px;">' +
+    '<span id="timer-minutes">00</span>:<span id="timer-seconds">00</span></h1>'
+var MESSAGES = ["Ничем не может человек распорядиться в большей степени, чем временем.",
+                "Ни река, ни быстротечное время остановиться не могут.",
+                "Как в море льются быстро воды, так в вечность льются дни и годы.",
+                "Счастливые часов не наблюдают.",
+                "Власть времени — это закон, достойный уважения."
+]
 
 // adds HTML tag to current page
 var timerContainer = document.createElement('div')
-timerContainer.setAttribute("style", "height: 100px;")
+timerContainer.setAttribute("style",
+    "height: 28px; top: 28px; padding: 3px; position: fixed; z-index: 101; " +
+    "background: #f2f2f2; opacity: 0.8;")
 var bodyTag = document.body
 bodyTag.insertBefore(timerContainer, bodyTag.firstChild)
 timerContainer.innerHTML = TEMPLATE
@@ -29,5 +39,9 @@ function displayTimer(){
 
   document.getElementById('timer-minutes').innerHTML = padZero(minutes)
   document.getElementById('timer-seconds').innerHTML = padZero(seconds)
+  if(secsLeft == 0 && secsGone % 30 == 0){
+      var message = MESSAGES[Math.floor(Math.random() * MESSAGES.length)];
+      alert(message);
+    }
 }
 setInterval(displayTimer, 300)
